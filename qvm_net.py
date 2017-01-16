45ec371c5a763314246280fb70f5a324b414ffc1
unman <unman@thirdeyesecurity.org>
2017-01-16 02:24:34 +0000

    Tidy up display'

--- qvm_net.py
+++ qvm_net.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 """
-Created on Sun Jan 15 23:02:34 2017
-
 Show network tree
 
 @author: unman
@@ -15,12 +13,12 @@ qvm_collection.unlock_db()
 qvm_collection.pop(0)
 
 def tree(netvm, padding):
-    padding = padding + '   |-> '
+    padding = padding + '    '
     for vm in netvm.connected_vms:
         if qvm_collection[vm].is_template():
-            print padding,qvm_collection[vm].name,'(Tpl)'
+            print padding,'|->',qvm_collection[vm].name,'(Tpl)'
         else:
-            print padding,qvm_collection[vm].name
+            print padding,'|->',qvm_collection[vm].name
         if qvm_collection[vm].is_netvm() :
             tree(qvm_collection[vm], padding)         
           
